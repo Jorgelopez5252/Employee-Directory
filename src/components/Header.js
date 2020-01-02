690ec79c74d39953a98b24e52ea79c5c21df11b4
Jorge Lopez <JorgeLopez5252@gmail.com>
2020-01-02 15:23:54 -0700

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 // Header should be a functional Component
 import React from "react";
 
+const divStyle = {
+    color: 'blue',
+    
+  };
+  
+
 function Header() {
-    return <h1>Welcome to my Employee Directory</h1>
+    return <h1 style ={divStyle}>Welcome to my Employee Directory</h1>
 }
 
 export default Header;
--- src/components/TableArea.js
+++ src/components/TableArea.js
@@ -7,8 +7,118 @@ import API from "../utils/API";
 
 class TableArea extends Component {
       state = {
-        result:[{}],
-        filtered: [{}]
+        result:[{
+          "gender": "female",
+          "name": {
+              "title": "Ms",
+              "first": "Lillian",
+              "last": "Beck"
+          },
+          "location": {
+              "street": {
+                  "number": 840,
+                  "name": "E Center St"
+              },
+              "city": "Santa Rosa",
+              "state": "Kansas",
+              "country": "United States",
+              "postcode": 77668,
+              "coordinates": {
+                  "latitude": "-50.8410",
+                  "longitude": "173.7384"
+              },
+              "timezone": {
+                  "offset": "+5:45",
+                  "description": "Kathmandu"
+              }
+          },
+          "email": "lillian.beck@example.com",
+          "login": {
+              "uuid": "90fb12c4-c4e7-47cd-85ba-a44e672c3b30",
+              "username": "redmouse346",
+              "password": "purple",
+              "salt": "p5EEUYtv",
+              "md5": "76b0436909e1eaa7c5e1d05b17fc6fdd",
+              "sha1": "094427f48176ed9c014e232c34c21e6e04a54961",
+              "sha256": "1882e4bdb83c4634048e100143cebd08c94816ca759df83139c9eef619b795ba"
+          },
+          "dob": {
+              "date": "1961-05-26T06:32:59.580Z",
+              "age": 58
+          },
+          "registered": {
+              "date": "2018-05-02T02:47:43.501Z",
+              "age": 1
+          },
+          "phone": "(417)-262-0093",
+          "cell": "(850)-983-1745",
+          "id": {
+              "name": "SSN",
+              "value": "919-27-7655"
+          },
+          "picture": {
+              "large": "https://randomuser.me/api/portraits/women/39.jpg",
+              "medium": "https://randomuser.me/api/portraits/med/women/39.jpg",
+              "thumbnail": "https://randomuser.me/api/portraits/thumb/women/39.jpg"
+          },
+          "nat": "US"
+      }],
+        filtered: [{
+          "gender": "female",
+          "name": {
+              "title": "Ms",
+              "first": "Lillian",
+              "last": "Beck"
+          },
+          "location": {
+              "street": {
+                  "number": 840,
+                  "name": "E Center St"
+              },
+              "city": "Santa Rosa",
+              "state": "Kansas",
+              "country": "United States",
+              "postcode": 77668,
+              "coordinates": {
+                  "latitude": "-50.8410",
+                  "longitude": "173.7384"
+              },
+              "timezone": {
+                  "offset": "+5:45",
+                  "description": "Kathmandu"
+              }
+          },
+          "email": "lillian.beck@example.com",
+          "login": {
+              "uuid": "90fb12c4-c4e7-47cd-85ba-a44e672c3b30",
+              "username": "redmouse346",
+              "password": "purple",
+              "salt": "p5EEUYtv",
+              "md5": "76b0436909e1eaa7c5e1d05b17fc6fdd",
+              "sha1": "094427f48176ed9c014e232c34c21e6e04a54961",
+              "sha256": "1882e4bdb83c4634048e100143cebd08c94816ca759df83139c9eef619b795ba"
+          },
+          "dob": {
+              "date": "1961-05-26T06:32:59.580Z",
+              "age": 58
+          },
+          "registered": {
+              "date": "2018-05-02T02:47:43.501Z",
+              "age": 1
+          },
+          "phone": "(417)-262-0093",
+          "cell": "(850)-983-1745",
+          "id": {
+              "name": "SSN",
+              "value": "919-27-7655"
+          },
+          "picture": {
+              "large": "https://randomuser.me/api/portraits/women/39.jpg",
+              "medium": "https://randomuser.me/api/portraits/med/women/39.jpg",
+              "thumbnail": "https://randomuser.me/api/portraits/thumb/women/39.jpg"
+          },
+          "nat": "US"
+      }]
       };
     
   
